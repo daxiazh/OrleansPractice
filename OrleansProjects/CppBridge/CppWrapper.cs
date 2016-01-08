b2c110ce654c9b125d2063fd86e49c56d70d30a9
barry <daxiazh@qq.com>
2016-01-08 16:48:00 +0800

    更新了 C++ Wrapper 的说明

--- OrleansProjects/CppBridge/CppWrapper.cs
+++ OrleansProjects/CppBridge/CppWrapper.cs
@@ -30,11 +30,12 @@ namespace CppBridge
         {
             // 现在先写死的路径,以方便 测试功能是否使,后面真正用到时,再写命令行来更灵活的处理绑定吧
             // 估计以后要区分 32位与64位
+            // 擦: 现在的 CppSharp 只支持32位,估计以后可能支持吧
             var options = driver.Options;
             options.GeneratorKind = GeneratorKind.CSharp;
             options.LibraryName = "Common";
             options.Headers.Add(@"D:\GitSourceCode\TempCode\XunLongJiMobileServer\Common\Include\TestExportClass.h");
-            options.Libraries.Add(@"D:\GitSourceCode\TempCode\XunLongJiMobileServer\Lib\x64\Debug\Common.lib");
+            options.Libraries.Add(@"D:\GitSourceCode\TempCode\XunLongJiMobileServer\Lib/Win32/Debug\Common.lib");
             options.ShowHelpText = true;
             options.OutputDir = @"d:\CppWrapper";
             options.OutputNamespace = "XLJCommon";
