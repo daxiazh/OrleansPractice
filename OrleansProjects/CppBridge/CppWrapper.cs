922eb9b29ad1d0413bc7b8734ea4af687254ee06
barry <daxiazh@qq.com>
2016-01-08 15:55:40 +0800

    添加用于生成 C++ 绑定到 C#的工程 CppBridge

---
+++ OrleansProjects/CppBridge/CppWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace CppBridge
+{
+    class CppWrapper : ILibrary
+    {
+        public void Postprocess(Driver driver, ASTContext ctx)
+        {
+           
+        }
+
+        public void Preprocess(Driver driver, ASTContext ctx)
+        {
+           
+        }
+
+        public void SetupPasses(Driver driver)
+        {
+           
+        }
+
+        void ILibrary.Setup(Driver driver)
+        {
+            // 现在先写死的路径,以方便 测试功能是否使,后面真正用到时,再写命令行来更灵活的处理绑定吧
+            var options = driver.Options;
+            options.GeneratorKind = GeneratorKind.CSharp;
+            options.LibraryName = "Common";
+            options.Headers.Add(@"D:\GitSourceCode\TempCode\XunLongJiMobileServer\Common\Include\TestExportClass.h");
+            options.Libraries.Add(@"D:\GitSourceCode\TempCode\XunLongJiMobileServer\Lib\x64\Debug\Common.lib");
+            options.ShowHelpText = true;
+            options.OutputDir = @"d:\CppWrapper";
+            options.OutputNamespace = "XLJCommon";
+        }        
+    }
+}
---
+++ OrleansProjects/CppBridge/Program.cs
@@ -0,0 +1,18 @@
+﻿using CppSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CppBridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 开始导出 C++ 对应的 C# wrapper
+            ConsoleDriver.Run(new CppWrapper());
+        }
+    }
+}
